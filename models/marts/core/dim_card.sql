6f66f3519a18f980f8745e1c976a510063e3c2bd
Javier Abril Román <javiar2612@gmail.com>
2025-11-28 02:32:13 +0000

    ay

--- models/marts/core/dim_card.sql
+++ models/marts/core/dim_card.sql
@@ -5,5 +5,10 @@ SELECT
     CARD_NAME,
     CARD_TYPE,
     COST,
-    IS_WINCON
+    IS_WINCON,
+    CONCAT(
+        'https://royaleapi.github.io/cr-api-assets/cards/',
+        REPLACE(LOWER(CARD_NAME), ' ', '-'), -- Transforma a minúsculas y reemplaza espacios por guiones
+        '.png'
+    ) AS CARD_IMAGE_URL
 FROM {{ ref("_stg_match_info__cards") }}
